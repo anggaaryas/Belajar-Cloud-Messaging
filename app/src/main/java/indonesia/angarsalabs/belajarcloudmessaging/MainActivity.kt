9e9cfa8c62f75a1b472c127af89e511c16867407
Angga Arya Saputra <anggaarya53@gmail.com>
2019-06-15 10:59:45 +0700

    Initial commit

---
+++ app/src/main/java/indonesia/angarsalabs/belajarcloudmessaging/MainActivity.kt
@@ -0,0 +1,34 @@
+package indonesia.angarsalabs.belajarcloudmessaging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w("AAAA", "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                // Log and toast
+                val msg = "INI PESAN" + token
+                Log.d("AAAA", msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            })
+
+    }
+}
